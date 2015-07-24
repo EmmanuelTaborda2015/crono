099d95639b16b4e3b5dc1798476f55eea09b3590
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-07-23 20:10:38 -0500

    Componente de nserción de parámetros a partir de bootstrap dialog

--- blocks/docentes/planDeTrabajo/script/fullCalendar/js/init.js
+++ blocks/docentes/planDeTrabajo/script/fullCalendar/js/init.js
@@ -8,28 +8,45 @@ $(document).ready(function() {
 
 function loadCalendar(data) {
 	var calendario = $('#calendar');
-//	var dayClickListener = function(date, jsEvent, view) {
-////		calendario.fullCalendar('changeView', 'agendaWeek');
-////		calendario.fullCalendar('gotoDate', date);
-//
-//		// change the day's background color just for fun
-//		$(this).css('background-color', 'red');
-//
-//	};
+	//	var dayClickListener = function(date, jsEvent, view) {
+	////		calendario.fullCalendar('changeView', 'agendaWeek');
+	////		calendario.fullCalendar('gotoDate', date);
+	//
+	//		// change the day's background color just for fun
+	//		$(this).css('background-color', 'red');
+	//
+	//	};
 	var selectListener = function(start, end) {
-		var title = prompt('Título de la actividad:');
-		
-		var eventData;
-		if (title) {
+		var onSave = function(contenido) {
+			var attr = $.juu.formToAttrObject(contenido);
 			eventData = {
-				title : title,
+				title : attr.nombreActividad,
 				start : start,
 				end : end
 			};
 			$('#calendar').fullCalendar('renderEvent', eventData, true);
 			// stick? = true
-		}
-		$('#calendar').fullCalendar('unselect');
+			$('#calendar').fullCalendar('unselect');
+		};
+		
+		var values = [{
+			'name' : 'nombreActividad',
+			'alias' : 'Nombre de la Actividad'
+		}, {
+			'name' : 'observacion',
+			'alias' : 'Observación'
+		}];
+
+		var contenido = $.juu.createFormWithSelects(values);
+		perro = contenido;
+		var options = {
+			titulo : 'Parámetros de la Actividad',
+			contenido : contenido,
+			onSave : onSave
+		};
+		var dialog = $.juu.createDialog(options);
+		dialog.modal('show');
+
 	};
 	calendario.fullCalendar({
 		theme : true,
@@ -39,38 +56,38 @@ function loadCalendar(data) {
 			right : 'today'
 		},
 		defaultDate : '2015-02-12',
-		defaultView: 'agendaWeek',
+		defaultView : 'agendaWeek',
 		lang : 'es',
-		slotDuration: '01:00:00',
+		slotDuration : '01:00:00',
 		// time formats
-//		titleFormat: {
-//		    month: 'MMMM yyyy',
-//		    week: "MMM d[ yyyy]{ '&#8212;'[ MMM] d yyyy}",
-//		    day: 'dddd, MMM d, yyyy'
-//		},
-//		columnFormat: {
-//		    month: 'ddd',
-//		    week: 'ddd M/d',
-//		    day: 'dddd d'
-//		},
-		axisFormat: 'h:mm A', //,'H(:mm)',
-		timeFormat: {
-			agenda: 'h:mm A' //H(:mm)'
-	    },
-	    height: 600,
+		//		titleFormat: {
+		//		    month: 'MMMM yyyy',
+		//		    week: "MMM d[ yyyy]{ '&#8212;'[ MMM] d yyyy}",
+		//		    day: 'dddd, MMM d, yyyy'
+		//		},
+		//		columnFormat: {
+		//		    month: 'ddd',
+		//		    week: 'ddd M/d',
+		//		    day: 'dddd d'
+		//		},
+		axisFormat : 'h:mm A', //,'H(:mm)',
+		timeFormat : {
+			agenda : 'h:mm A' //H(:mm)'
+		},
+		height : 610,
 		// locale
-//		buttonIcons : false, // show the prev/next text
+		//		buttonIcons : false, // show the prev/next text
 		// weekNumbers: true,
-//		businessHours : true, // display business hours
+		//		businessHours : true, // display business hours
 		editable : true,
 		allDaySlot : false,
 		allDayText : false,
-//		eventLimit : true, // allow "more" link when too many events
+		//		eventLimit : true, // allow "more" link when too many events
 		events : data.events,
-//		dayClick : dayClickListener,
+		//		dayClick : dayClickListener,
 		selectable : true,
 		selectHelper : true,
 		select : selectListener
 	});
-	
+
 }
--- blocks/docentes/planDeTrabajo/script/fullCalendar/js/juu.js
+++ blocks/docentes/planDeTrabajo/script/fullCalendar/js/juu.js
@@ -7,35 +7,38 @@
 
 $.juu = new Object();
 
-$.juu.crearDivPorClase = function(clase) {
+$.juu.createDivByClass = function(clase) {
 	return $("<div>")
 	.addClass(clase);
-}
-$.juu.crearButtonPorClase = function(clase) {
+};
+
+$.juu.createButtonByClass = function(clase) {
 	return $("<button>")
 	.attr("type","button")
 	.addClass(clase);
 }
-$.juu.crearDialogo = function(obj) {
+
+$.juu.createDialog = function(obj) {
 	obj=(obj!=null)?obj:new Object();
 	obj.titulo=(obj.titulo!=null)?obj.titulo:"Mi título";
 	obj.closeMessage=(obj.closeMessage!=null)?obj.closeMessage:"Cerrar";
 	obj.saveMessage=(obj.saveMessage!=null)?obj.saveMessage:"Guardar Actividad";
 	obj.contenido=(obj.contenido!=null)?obj.contenido:"Mi contenido";
+	obj.onSave=(obj.onSave!=null)?obj.onSave:function(){};
 	
-	var modalFade = $.juu.crearDivPorClase("modal fade")
+	var modalFade = $.juu.createDivByClass("modal fade")
 	.attr("id","dialog-confirm");
 	
-	var modalDialog = $.juu.crearDivPorClase("modal-dialog")
+	var modalDialog = $.juu.createDivByClass("modal-dialog")
 	.appendTo(modalFade);
 	
-	var modalContent = $.juu.crearDivPorClase("modal-content")
+	var modalContent = $.juu.createDivByClass("modal-content")
 	.appendTo(modalDialog);
 	
-	var modalHeader = $.juu.crearDivPorClase("modal-header")
+	var modalHeader = $.juu.createDivByClass("modal-header")
 	.appendTo(modalContent);
 	
-	var modalHeaderButton1 = $.juu.crearButtonPorClase("close")
+	var modalHeaderButton1 = $.juu.createButtonByClass("close")
 	.attr("data-dismiss","modal")
 	.attr("aria-hidden","true")
 	.html("x")
@@ -46,32 +49,59 @@ $.juu.crearDialogo = function(obj) {
 	.html(obj.titulo)
 	.appendTo(modalHeader);
 	
-	var modalBody = $.juu.crearDivPorClase("modal-body")
+	var modalBody = $.juu.createDivByClass("modal-body")
 	.appendTo(modalContent);
 	
 	var modalBodyP = $("<p>")
-	.html(obj.contenido)
 	.appendTo(modalBody);
 	
-	var modalFooter = $.juu.crearDivPorClase("modal-footer")
+	if(typeof(obj.contenido)=='object'){
+		modalBodyP.append(obj.contenido);
+	} else {
+		modalBodyP.html(obj.contenido);
+	}
+	
+	var modalFooter = $.juu.createDivByClass("modal-footer")
 	.appendTo(modalContent);
 	
-	var modalFooterButton1 = $.juu.crearButtonPorClase("btn btn-default")
+	var modalFooterButton1 = $.juu.createButtonByClass("btn btn-default")
 	.attr("data-dismiss","modal")
 	.html(obj.closeMessage)
 	.appendTo(modalFooter);
 	
-	var modalFooterButton2 = $.juu.crearButtonPorClase("btn btn-primary")
+	var modalFooterButton2 = $.juu.createButtonByClass("btn btn-primary")
 	.html(obj.saveMessage)
+	.click(function(){
+		obj.onSave(obj.contenido);
+		modalFade.modal('hide');
+	})
 	.appendTo(modalFooter);
 	
-	modalFade.modal('show');
+	modalFade.modal();
 	
     return modalFade;
 };
 
-$.juu.fn = function() {
-    // Close popup code.
+$.juu.createFormWithSelects = function(values) {
+    return $("<form>").html(
+		function (){
+			//values is objects with key value attributes				
+			var str = new String();
+			$.each(values, function (i,v){
+				str += this.alias+":<br/><input type='text' name='"+this.name+"'>";
+				str += (i<values.length)?'<br/>':'';		
+			});
+			return str;
+		}()
+	);
+};
+
+$.juu.formToAttrObject = function(form) {
+    var obj = new Object();
+    $(form).find("[name]").each(function(index){
+    	obj[$(this).attr('name')] = $(this).val();
+    });
+    return obj;
 };
  
 }( jQuery ));
